478e9c5a16fb99e9fe8bf4262b674a33d2def7f7
maik <maik.zimpel@gmail.com>
2018-09-23 22:54:18 +0200

    add support for spek tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,17 @@
+import org.gradle.internal.impldep.aQute.bnd.osgi.resource.Filters
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+buildscript {
+    dependencies {
+        classpath("org.junit.platform:junit-platform-gradle-plugin:1.0.0")
+    }
+}
+
+
+apply {
+    plugin("org.junit.platform.gradle.plugin")
+}
+
 
 plugins {
     kotlin("jvm") version "1.2.51"
@@ -16,17 +29,17 @@ kotlin {
 repositories {
     jcenter()
     mavenCentral()
-    maven {setUrl("http://dl.bintray.com/kotlin/ktor")}
     maven {setUrl("http://dl.bintray.com/kotlin/kotlinx")}
+    maven {setUrl("http://dl.bintray.com/jetbrains/spek")}
 }
 
 dependencies {
     compile(kotlin("stdlib-jdk8"))
-    compile("io.ktor:ktor:0.9.5")
-    compile("io.ktor:ktor-html-builder:0.9.5")
-    compile("io.ktor:ktor-gson:0.9.5")
-    compile("io.ktor:ktor-server-netty:0.9.5")
-    compile("io.ktor:ktor-client-core:0.9.5")
+    compile("khttp:khttp:0.1.0")
+    testCompile("org.jetbrains.spek:spek-api:1.1.5")
+    testCompile("junit:junit:4.11")
+    testCompile("org.assertj:assertj-core:3.4.1")
+    testRuntime("org.jetbrains.spek:spek-junit-platform-engine:1.1.5")
 }
 
 tasks.withType<KotlinCompile> {
--- src/main/kotlin/eu/lumpy/camptracker/esi/EsiClient.kt
+++ src/main/kotlin/eu/lumpy/camptracker/esi/EsiClient.kt
@@ -1,6 +1,17 @@
 package eu.lumpy.camptracker.esi
 
+import khttp.get
+import khttp.responses.Response
 
-suspend fun requestKmsForAlliance(allianceId: Int ) {
+class EsiClient(val baseUrl: String) {
+
+    fun requestCorporationsForAlliance(allianceId: Int): Response {
+        return get("https://" + baseUrl + "/alliances/" + allianceId + "/corporations/?datasource=tranquility")
+    }
+
+    fun requestKillmailsforCorporation(corporationsId: Int): Response {
+        return get("https://" + baseUrl + "/corporations/" + corporationsId + "/killmails/recent/?datasource=tranquility")
+    }
 
 }
+
